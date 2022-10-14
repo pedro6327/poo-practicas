41bd015443bd08d7c7537ffff5b70288db238065
PEDRO <PEDRO@DESKTOP-RCSE3UI>
2022-10-14 12:38:00 -0500

    Practica 4

--- src/main/java/p61/Auto.java
+++ src/main/java/p61/Auto.java
@@ -16,184 +16,240 @@ public class Auto {
     int anio;
     String color;
     double precio;
-    
-            
-    public String mostrarInfo(){
-        var retorno="El auto tiene placa: "+this.placa+" es marca: "+this.marca+" el propietario es:  "
-                +this.nombrePropietario+"  El año del auto es: "+this.anio ;
-        
-        return retorno;          
-    }
-    public String getProvincia(){
-        
+    int cilindrajeMotor;
 
-        var retorno="";
-        var primeraLetra="";
-        primeraLetra= this.placa.substring(0,1);
+    public String mostrarInfo() {
+        var retorno = "El auto tiene placa: " + this.placa + " es marca: " + this.marca + " el propietario es:  "
+                + this.nombrePropietario + "  El año del auto es: " + this.anio;
+
+        return retorno;
+    }
+
+    public String getProvincia() {
+
+        var retorno = "";
+        var primeraLetra = "";
+        primeraLetra = this.placa.substring(0, 1);
         switch (primeraLetra) {
             case "A":
-                retorno="Azuay";
+                retorno = "Azuay";
                 break;
-                case "P":
-                retorno="Pichincha";
+            case "P":
+                retorno = "Pichincha";
                 break;
-                case "G":
-                retorno="Guayas";
+            case "G":
+                retorno = "Guayas";
                 break;
-                case "U":
-                retorno="Cañar";
+            case "U":
+                retorno = "Cañar";
                 break;
-                case "L":
-                retorno="Loja";
-                break;
-                default:
-                  retorno="Provincia NO definida";   
-        }
-        return retorno;          
-    }
-    
-    public String getPaisMarca(){
-        var retorno="";
-        if(this.marca.equals("Chevrolet")){
-        retorno="EEUU";
-        }
-        else {
-            if (this.marca.equals("Toyota")) {
-             retorno="Japon";   
-            }
-            else {
-                if (this.marca.equals("Jettour")) {
-             retorno="Chino";   
-            }
-                else {
-                      if (this.marca.equals("Audi")) {
-             retorno="Aleman";   
-            }
-                      
-                      else {
-                            retorno="ND"; 
-                      }
-                }
-                
-            
-                     }
-          
-      
-                }
-        return retorno;          
-    }
-    public int getNumerocontinente(){
-        // 1 africa 2 america 3 asia 4 europa 5 oceania
-       var marcaPais =this.getPaisMarca();
-        var retorno=0;
-        switch (marcaPais) {
-            case "EEUU":
-               retorno=2;
-                break;
-                case "Japon":
-               retorno=3 ;
-                break;
-                case "Jettour":
-               retorno=3 ;
-                break;
-                case "Aleman":
-               retorno=4 ;
+            case "L":
+                retorno = "Loja";
                 break;
             default:
-                retorno=0 ;
-                
+                retorno = "Provincia NO definida";
         }
-        
-        
-        
-        return retorno;          
+        return retorno;
     }
-    
-     public double getIva(){
-        var retorno=0.0d;
-        retorno = this.precio*0.12;
-        return retorno;          
+
+    public String getPaisMarca() {
+        var retorno = "";
+        if (this.marca.equals("Chevrolet")) {
+            retorno = "EEUU";
+        } else {
+            if (this.marca.equals("Toyota")) {
+                retorno = "Japon";
+            } else {
+                if (this.marca.equals("Jettour")) {
+                    retorno = "Chino";
+                } else {
+                    if (this.marca.equals("Audi")) {
+                        retorno = "Aleman";
+                    } else {
+                        retorno = "ND";
+                    }
+                }
+
+            }
+
+        }
+        return retorno;
     }
-   
-     public double getCostomatricula(){
-         /*edad de 0-5 años 10% precio si es menos de 10000 si se asegura 
+
+    public int getNumerocontinente() {
+        // 1 africa 2 america 3 asia 4 europa 5 oceania
+        var marcaPais = this.getPaisMarca();
+        var retorno = 0;
+        switch (marcaPais) {
+            case "EEUU":
+                retorno = 2;
+                break;
+            case "Japon":
+                retorno = 3;
+                break;
+            case "Jettour":
+                retorno = 3;
+                break;
+            case "Aleman":
+                retorno = 4;
+                break;
+            default:
+                retorno = 0;
+
+        }
+
+        return retorno;
+    }
+
+    public double getIva() {
+        var retorno = 0.0d;
+        retorno = this.precio * 0.12;
+        return retorno;
+    }
+
+    public double getCostomatricula() {
+        /*edad de 0-5 años 10% precio si es menos de 10000 si se asegura 
          edad de 5-15 años 10% precio si es menos de 10000 a 20000 si se asegura 
          edad de 15-nayor años 10% precio si es menos de 20000 a 30000 si se asegura 
          */
-        var retorno=0.0d;
-         if (this.getEdad(2022) > 0 && this.getEdad(2022)<=5) {
-             retorno=this.precio*0.1;
-         }
-         else{
-             if (this.getEdad(2022) > 5 && this.getEdad(2022)<=10) {
-             retorno=this.precio*0.08;
-         }
-             else{
-                 if (this.getEdad(2022) > 10 && this.getEdad(2022)<=15) {
-             retorno=this.precio*0.06;
-         }
-                 else {
-                     if (this.getEdad(2022) > 15 ) {
-             retorno=this.precio*0.05;
-         }
-                 }
-             }
-         }
-          return retorno;       
-    }   
-     
-     public boolean esTaxi(){
-        var retorno=true;
-        return retorno;          
-    }      
-     
-     
-     
-       public boolean seAsegura(int anioActual){
-        var retorno=false;
-          var edad = this.getEdad(anioActual);
-           if (edad<=5 && this.getCostomatricula()<10000) {
-               retorno=true;
-           }
-           else{
-               if (edad>5 && edad<=15  && this.getCostomatricula()>10000 && this.getCostomatricula()<20000) {
-               retorno=true;
-           }
-               else{
-                    if ( edad>15 ) {
-               retorno=false;
-           }
-               }
-              
-           }
-        return retorno;          
-    }  
-       
-        
-      public boolean esClasico(){
-        var retorno=false;
-      
-        return retorno;          
-    }   
-      
-      public int getEdad(int anioActual){
-        var retorno=0;
-        retorno= anioActual- this.anio;
-        return retorno;          
-    }  
-      //depreciasion no puede exceder el precio 
-      // 10 por años
-       public double getDepreciasion(int anioActual){
-        var retorno=0.0d;
-        var edad = this.getEdad(anioActual);
-        retorno = edad*0.1*this.precio;
-        return retorno;          
+        var retorno = 0.0d;
+        if (this.getEdad(2022) > 0 && this.getEdad(2022) <= 5) {
+            retorno = this.precio * 0.1;
+        } else {
+            if (this.getEdad(2022) > 5 && this.getEdad(2022) <= 10) {
+                retorno = this.precio * 0.08;
+            } else {
+                if (this.getEdad(2022) > 10 && this.getEdad(2022) <= 15) {
+                    retorno = this.precio * 0.06;
+                } else {
+                    if (this.getEdad(2022) > 15) {
+                        retorno = this.precio * 0.05;
+                    }
+                }
+            }
+        }
+        return retorno;
     }
-      
-      public boolean esPLacaValida(){
-        var retorno=true;
-        return retorno;          
-    }    
-      
+
+    public boolean esTaxi() {
+        var retorno = true;
+        return retorno;
+    }
+
+    public boolean seAsegura(int anioActual) {
+        var retorno = false;
+        var edad = this.getEdad(anioActual);
+        if (edad <= 5 && this.getCostomatricula() < 10000) {
+            retorno = true;
+        } else {
+            if (edad > 5 && edad <= 15 && this.getCostomatricula() > 10000 && this.getCostomatricula() < 20000) {
+                retorno = true;
+            } else {
+                if (edad > 15) {
+                    retorno = false;
+                }
+            }
+
+        }
+        return retorno;
+    }
+
+    public boolean esClasico(int anioActual, int anioClasico ) {
+        var retorno = false;
+
+        return retorno;
+    }
+ public double getTasaSolidaria(int anioActual) {
+     /*
+     edad>20 tasa=0
+     edad 0-5 y cilindraje <1500 tasa =15%costo
+      edad 0-5 y cilindraje >1500 <2000 tasa =20%costo
+     edad 0-5 y cilindraje >2000 tasa =25%costo
+     edad 5-15 y cilindraje <1500 tasa =10%costo
+      edad 5-15 y cilindraje >1500 <2000 tasa =15%costo
+     edad 5-15 y cilindraje >2000 tasa =18%costo
+     edad 15-20 tasa =5%costo
+      edad >20 tasa =5%costo
+    
+     */
+        var retorno = 0.0d;
+        var edad = this.getEdad(anioActual);
+        if (edad <= 5 && this.cilindrajeMotor<1500) {
+            retorno = this.precio*0.15;
+        } else{
+            if (edad <= 5 && this.cilindrajeMotor>1500 && this.cilindrajeMotor<2000) {
+            retorno = this.precio*0.2;
+        } else{ if (edad <= 5 && this.cilindrajeMotor>2000 ) {
+            retorno = this.precio*0.25;
+        } else{ if (edad > 5 && edad <= 15 && this.cilindrajeMotor<1500 ) {
+            retorno = this.precio*0.1;
+   
+        } else{ if (edad > 5 && edad <= 15 && this.cilindrajeMotor>1500 && this.cilindrajeMotor<2000) {
+            retorno = this.precio*0.15;
+        } else{ if (edad > 5 && edad <= 15 && this.cilindrajeMotor>2000) {
+            retorno = this.precio*0.18;  
+        } else{if (edad > 15 && edad <= 20 ) {
+            retorno = this.precio*0.05;     
+        }else{if (edad > 20 ) {
+            retorno = this.precio*0.03;  
+        }
+        
+        }
+        }
+        }
+        
+        }
+            
+            }
+        
+        }
+        
+    }
+        return retorno;
+ }
+ 
+ 
+    public int getEdad(int anioActual) {
+        var retorno = 0;
+        retorno = anioActual - this.anio;
+        return retorno;
+    }
+    //depreciasion no puede exceder el precio 
+    // 10 por años
+
+    public double getDepreciasion(int anioActual) {
+        var retorno = 0.0d;
+        var edad = this.getEdad(anioActual);
+        retorno = edad * 0.1 * this.precio;
+        return retorno;
+    }
+
+    public boolean esPLacaValida() {
+        var retorno = false;
+        var longitud=this.placa.length();
+        if (longitud==7) {
+            var char1=this.placa.charAt(0);
+            var char2=this.placa.charAt(1);
+            var char3=this.placa.charAt(2);
+            var char4=this.placa.charAt(3);
+            var char5=this.placa.charAt(4);
+            var char6=this.placa.charAt(5);
+            var char7=this.placa.charAt(6);
+            
+            if (Character.isLetter(char1) && Character.isLetter(char2)&& Character.isLetter(char3)
+                &&  Character.isDigit(char4)&&  Character.isDigit(char5)&&  Character.isDigit(char6) ) {
+                retorno= true;
+            }
+            
+           
+        }
+        
+        
+        
+        return retorno;
+    }
+    
+    
+
 }
 
--- src/main/java/p61/Practica3.java
+++ src/main/java/p61/Practica3.java
@@ -11,85 +11,94 @@ package p61;
 public class Practica3 {
     
  public static void main(String[] args) {
+   
         System.out.println("Hello World!");
-        
-    
-        
-         var auto1 = new Auto ();
-         var auto2 = new Auto ();
-         var auto3 = new Auto ();
-         var auto4 = new Auto ();
-         var auto5 = new Auto ();
-         
+
+        var auto1 = new Auto();
+        var auto2 = new Auto();
+        var auto3 = new Auto();
+        var auto4 = new Auto();
+        var auto5 = new Auto();
+
         auto1.marca = "Toyota";
-        auto1.placa = "GDL-207";
+        auto1.placa = "GDL0207";
         auto1.nombrePropietario = "Karla";
         auto1.anio = 2016;
         auto1.color = "rojo";
         auto1.precio = 20000;
-        
-                
-                
-        auto2.marca = "Chevrolet";        
-        auto2.placa = "ABL-307";
+        auto1.cilindrajeMotor=1600;
+
+        auto2.marca = "Chevrolet";
+        auto2.placa = "ABL0307";
         auto2.nombrePropietario = "Pedro";
         auto2.anio = 2018;
         auto2.color = "azul";
         auto2.precio = 30000;
-        
-         auto3.marca = "Jettour";        
-        auto3.placa = "PVL-307";
+        auto2.cilindrajeMotor=1000;
+
+        auto3.marca = "Jettour";
+        auto3.placa = "PVL0307";
         auto3.nombrePropietario = "Andres";
         auto3.anio = 2015;
         auto3.color = "rojo";
         auto3.precio = 70000;
-        
-        auto4.marca = "Audi";        
-        auto4.placa = "UUB-847";
+        auto3.cilindrajeMotor=1900;
+
+        auto4.marca = "Audi";
+        auto4.placa = "UUB0847";
         auto4.nombrePropietario = "Juan";
         auto4.anio = 2002;
         auto4.color = "rojo";
         auto4.precio = 10000;
-        
-        auto5.marca = "Lada";        
-        auto5.placa = "LHY-577";
+        auto4.cilindrajeMotor=2000;
+
+        auto5.marca = "Lada";
+        auto5.placa = "LHY0577";
         auto5.nombrePropietario = "Raul";
         auto5.anio = 2008;
         auto5.color = "rojo";
         auto5.precio = 40000;
-  
+        auto5.cilindrajeMotor=2200;
+
         System.out.println(auto2.mostrarInfo());
-        System.out.println("El pais de de origen es "+auto2.getPaisMarca()+" la provincia es "
-                + auto2.getProvincia()+" el auto tiene :"+auto2.getEdad(2022)+" años"
-        +" El iva del auto es: "+auto2.getIva() + " El costo de la matricula es "+ auto2.getCostomatricula());
-        System.out.println("el carro esta asegurado "+ auto2.seAsegura(2022));
+        System.out.println("El pais de de origen es " + auto2.getPaisMarca() + " la provincia es "
+                + auto2.getProvincia() + " el auto tiene :" + auto2.getEdad(2022) + " años"
+                + " El iva del auto es: " + auto2.getIva() + " El costo de la matricula es " + auto2.getCostomatricula());
+        System.out.println("el carro esta asegurado " + auto2.seAsegura(2022));
+        System.out.println("la placa es valida:  " + auto2.esPLacaValida());
+        System.out.println("la Tasa Solidaria es   " + auto2.getTasaSolidaria(2022));
         
+
         System.out.println(auto1.mostrarInfo());
-        System.out.println("El pais de de origen es "+auto1.getPaisMarca()+" la provincia es "
-                + auto1.getProvincia()+" el auto tiene :"+auto1.getEdad(2022)+" años"
-        +"El iva del auto es: "+auto1.getIva() + " El costo de la matricula es "+ auto1.getCostomatricula());
-        System.out.println("el carro esta asegurado "+ auto1.seAsegura(2022));
-        
+        System.out.println("El pais de de origen es " + auto1.getPaisMarca() + " la provincia es "
+                + auto1.getProvincia() + " el auto tiene :" + auto1.getEdad(2022) + " años"
+                + "El iva del auto es: " + auto1.getIva());
+        System.out.println("la placa es valida:  " + auto1.esPLacaValida());
+         System.out.println("la Tasa Solidaria es   " + auto1.getTasaSolidaria(2022));
+
         System.out.println(auto3.mostrarInfo());
-        System.out.println("El pais de de origen es "+auto3.getPaisMarca()+" la provincia es "
-                + auto3.getProvincia()+" el auto tiene :"+auto3.getEdad(2022)+" años"
-        +"El iva del auto es: "+auto3.getIva()+ " El costo de la matricula es "+ auto3.getCostomatricula());
-        System.out.println("el carro esta asegurado "+ auto3.seAsegura(2022));
-        
+        System.out.println("El pais de de origen es " + auto3.getPaisMarca() + " la provincia es "
+                + auto3.getProvincia() + " el auto tiene :" + auto3.getEdad(2022) + " años"
+                + "El iva del auto es: " + auto3.getIva());
+        System.out.println("la placa es valida:  " + auto3.esPLacaValida());
+         System.out.println("la Tasa Solidaria es   " + auto3.getTasaSolidaria(2022));
+
         System.out.println(auto4.mostrarInfo());
-        System.out.println("El pais de de origen es "+auto4.getPaisMarca()+" la provincia es "
-                + auto4.getProvincia()+" el auto tiene :"+auto4.getEdad(2022)+" años"
-        +"El iva del auto es: "+auto4.getIva()+ " El costo de la matricula es "+ auto4.getCostomatricula());
-        System.out.println("el carro esta asegurado "+ auto4.seAsegura(2022));
-        
+        System.out.println("El pais de de origen es " + auto4.getPaisMarca() + " la provincia es "
+                + auto4.getProvincia() + " el auto tiene :" + auto4.getEdad(2022) + " años"
+                + "El iva del auto es: " + auto4.getIva());
+        System.out.println("la placa es valida:  " + auto4.esPLacaValida());
+         System.out.println("la Tasa Solidaria es   " + auto4.getTasaSolidaria(2022));
+
         System.out.println(auto5.mostrarInfo());
-        System.out.println("El pais de de origen es "+auto5.getPaisMarca()+" la provincia es "
-                + auto5.getProvincia()+" el auto tiene :"+auto5.getEdad(2022)+" años"
-        +"El iva del auto es: "+auto5.getIva()+ " El costo de la matricula es "+ auto5.getCostomatricula());
-        System.out.println("el carro esta asegurado "+ auto5.seAsegura(2022));
-       
+        System.out.println("El pais de de origen es " + auto5.getPaisMarca() + " la provincia es "
+                + auto5.getProvincia() + " el auto tiene :" + auto5.getEdad(2022) + " años"
+                + "El iva del auto es: " + auto5.getIva());
+        System.out.println("la placa es valida:  " + auto5.esPLacaValida());
+         System.out.println("la Tasa Solidaria es   " + auto5.getTasaSolidaria(2022));
+
     }
-        
-    }
+
+}
 
 
