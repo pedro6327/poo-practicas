ce0860b5151c2692ed7ad4d993a90645f734f74f
PEDRO <PEDRO@DESKTOP-RCSE3UI>
2022-10-19 12:42:46 -0500

    Tarea 0103

--- src/main/java/p61/Tarea0102/Principal2.java
+++ src/main/java/p61/Tarea0102/Principal2.java
@@ -11,26 +11,24 @@ import java.util.Scanner;
  * @author PEDRO
  */
 public class Principal2 {
-     public static void main(String[] args) {
-         
-         var entrada = new Scanner(System.in);
-         System.out.println("ingrese la dimension del arreglo");
-         var dimension = entrada.nextInt();
-         var arreglo = new int[dimension];
-         
-         for (int i = 0; i < dimension; i++) {
-             System.out.println("Ingrese los datos del vector en la poscicion " +i);
-             var numero = entrada.nextInt();
-  
-             arreglo[i]=numero;
-         }
-         System.out.println("el arreglo ingresado es: ");
-         for (int i = 0; i < dimension; i++) {
-             System.out.println(arreglo[i]);
-         }
-         
-        
-         
-         
-     }
+
+    public static void main(String[] args) {
+
+        var entrada = new Scanner(System.in);
+        System.out.println("ingrese la dimension del arreglo");
+        var dimension = entrada.nextInt();
+        var arreglo = new int[dimension];
+
+        for (int i = 0; i < dimension; i++) {
+            System.out.println("Ingrese los datos del vector en la poscicion " + i);
+            var numero = entrada.nextInt();
+
+            arreglo[i] = numero;
+        }
+        System.out.println("el arreglo ingresado es: ");
+        for (int i = 0; i < dimension; i++) {
+            System.out.println(arreglo[i]);
+        }
+
+    }
 }
